9c5371d0c8b7beed67bc3db1bed46084ee468a6d
WILJA <wcbastos88@gmail.com>
2021-06-23 18:11:50 -0300

    Poc qrcode

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+f
+6src/main/java/com/qrcode/qrcode/QrcodeApplication.java,3/e/3e8ffc82d86d4343a5ef5e7ca56f428d70d0e259
---
+++ src/main/java/com/qrcode/qrcode/controller/QrCodeController.java
@@ -0,0 +1,51 @@
+package com.qrcode.qrcode.controller;
+
+
+import com.qrcode.qrcode.utils.QrCodeUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+
+@Controller
+public class QrCodeController {
+
+    @RequestMapping(value = "/createCommonQRCode")
+    public void createCommonQRCode(HttpServletResponse response, String url) throws Exception {
+        ServletOutputStream stream = null;
+        try {
+            stream = response.getOutputStream();
+
+            QrCodeUtil.encode(url, stream);
+        } catch (Exception e) {
+            e.getStackTrace();
+        } finally {
+            if (stream != null) {
+                stream.flush();
+                stream.close();
+            }
+        }
+    }
+
+
+    @RequestMapping(value = "/createLogoQRCode")
+    public void createLogoQRCode(HttpServletResponse response, String url) throws Exception {
+        ServletOutputStream stream = null;
+        try {
+            stream = response.getOutputStream();
+            String logoPath = Thread.currentThread().getContextClassLoader().getResource("").getPath()
+                    + "templates" + File.separator + "nexti.jpg";
+
+            QrCodeUtil.encode(url, logoPath, stream, true);
+        } catch (Exception e) {
+            e.getStackTrace();
+        } finally {
+            if (stream != null) {
+                stream.flush();
+                stream.close();
+            }
+        }
+    }
+}
